89e0073dba091321be5fdc443d89de62ade9ebbb
Bader Szabolcs <sb@baader.hu>
2021-03-02 14:00:27 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to microscope
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/microscope/core_test.clj
@@ -0,0 +1,7 @@
+(ns microscope.core-test
+  (:require [clojure.test :refer :all]
+            [microscope.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
